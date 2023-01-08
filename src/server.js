b16acbf413d5a81c31285d26ced73b65b8a1da5e
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2023-01-09 01:41:37 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -63,7 +63,8 @@ router.post('/', async (request, env) => {
         });
       }
       case INVITE_COMMAND.name.toLowerCase(): {
-        //const applicationId = env.DISCORD_APPLICATION_ID;
+        const applicationId = env.DISCORD_APPLICATION_ID;
+        console.log(applicationId);
         const INVITE_URL = `https://awwbot.pages.dev/invite`;
         return new JsonResponse({
           type: 4,
